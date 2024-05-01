dbc822a7811ab14a5349dbb9697cc1d6ebb59e44
Gagan714 <gaganursmg@gmail.com>
2024-05-01 18:06:57 +0530

    commit

--- chaiwithsky/app/api/auth/[...nextauth]/route.js
+++ chaiwithsky/app/api/auth/[...nextauth]/route.js
@@ -3,10 +3,15 @@ import AppleProvider from 'next-auth/providers/apple'
 import FacebookProvider from 'next-auth/providers/facebook'
 import GoogleProvider from 'next-auth/providers/google'
 import EmailProvider from 'next-auth/providers/email'
+import GitHubProvider from "next-auth/providers/github";
 
-export default NextAuth({
+export const authoptions= NextAuth({
   providers: [
     // OAuth authentication providers...
+    GitHubProvider({
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET
+      }),
     AppleProvider({
       clientId: process.env.APPLE_ID,
       clientSecret: process.env.APPLE_SECRET
@@ -26,3 +31,4 @@ export default NextAuth({
     }),
   ]
 })
+export {authoptions as GetAnimationsOptions,authoptions as POST}
