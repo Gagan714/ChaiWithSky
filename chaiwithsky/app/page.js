bfff1abd7343b28a6678d440f19219a93a93fbd5
Gagan714 <gaganursmg@gmail.com>
2024-05-01 14:04:12 +0530

    buttons

--- chaiwithsky/app/page.js
+++ chaiwithsky/app/page.js
@@ -11,7 +11,30 @@ export default function Home() {
       <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Read More</button>
       </div>
     </div>
-    <div className="h-1 bg-slate-50 opacity-10"></div>
+    <div className="h-1 bg-slate-50 opacity-10 mb-6"></div>
+    <div className="flex flex-col gap-4 justify-center items-center text-white">
+      <div className="font-bold text-xl">Your Fans Can buy You a Chai</div>
+      <div className="flex flex-row justify-around w-full items-center">
+        <div className="flex flex-col gap-2 justify-center items-center">
+        <div>
+          <Image className=" bg-slate-300 rounded-full" src="/man.jpg" alt="" width={80} height={80}/>
+        </div>
+        <div>Fund yourself</div>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+        <div>
+          <Image className="rounded-full" src="/Dollar.gif" alt="" width={80} height={80}/>
+        </div>
+        <div>Fund yourself</div>
+        </div>
+        <div className="flex flex-col gap-2 justify-center items-center">
+        <div>
+          <Image className=" bg-white rounded-full" src="/group.svg" alt="" width={80} height={80}/>
+        </div>
+        <div>Fund yourself</div>
+        </div>
+      </div>
+    </div>
     </>
   );
 }
