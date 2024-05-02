34568ad512d47408e2e95dbcbdeec8ddd757dbdf
Gagan714 <gaganursmg@gmail.com>
2024-05-02 09:31:01 +0530

    dashboard

---
+++ chaiwithsky/app/[username]/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Username = (params) => {
+  return (
+<div className="text-white">{params.username}</div>
+  )
+}
+
+export default Username
--- chaiwithsky/components/Navbar.js
+++ chaiwithsky/components/Navbar.js
@@ -5,17 +5,13 @@ import Link from 'next/link'
 
 const Navbar = () => {
   const { data: session } = useSession()
-  if(session) {
-    return <>
-      Signed in as {session.user.email} <br/>
-      <button onClick={() => signOut()}>Sign out</button>
-    </>
-  }
   return (
     <div className="bg-gray-900 text-white flex justify-between items-center h-12 px-5">
         <div className="logo font-bold text-xl">ChaiWithSky</div>
         <div>
-        <Link href={"/login"}><button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2">LogIn</button></Link>
+          {session && <Link href={"/dashboard"}><button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2">DashBoard</button></Link>}
+          {session && <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2" onClick={()=>{signOut()}}>LogOut</button>}
+        {!session &&<Link href={"/login"}><button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2">LogIn</button></Link>}
         </div>
     </div>
   )
