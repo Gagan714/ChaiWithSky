ff51a00716413b14058b6239033b8517a84fce3d
Gagan714 <gaganursmg@gmail.com>
2024-05-04 11:06:48 +0530

    error fix

--- chaiwithsky/app/page.js
+++ chaiwithsky/app/page.js
@@ -7,8 +7,8 @@ export default function Home() {
       <div className="text-3xl font-bold">Buy Me a Chai</div>
       <p>A Crowd funding Platform for Creators.Get funded by your Fans and Followers.Start now!</p>
       <div>
-      <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Here</button>
-      <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Read More</button>
+      <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Here</button>
+      <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Read More</button>
       </div>
     </div>
     <div className="h-1 bg-slate-50 opacity-10 mb-6"></div>
