08f697513885c54367d464fcacdcc81626bc4b55
Gagan714 <gaganursmg@gmail.com>
2024-05-08 16:15:16 +0530

    commit true

--- chaiwithsky/app/api/razorpay/route.js
+++ chaiwithsky/app/api/razorpay/route.js
@@ -14,7 +14,7 @@ export const POST=async (req)=>{
     let xx=validatePaymentVerification({"order_id": body.razorpay_order_id,"payment_id":body.razorpay_payment_id},body.razorpay_signature,process.env.KEY_SECRET)
 
     if(xx){
-        const updatedPayment=await Payment.findOneAndUpdate({oid:body.razorpay_order_id},{done:"true"},{new:true})
+        const updatedPayment=await Payment.findOneAndUpdate({oid:body.razorpay_order_id},{done:true},{new:true})
         return NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL}/${updatedPayment.to_user}?paymentdone=true`)
     }
     else{
