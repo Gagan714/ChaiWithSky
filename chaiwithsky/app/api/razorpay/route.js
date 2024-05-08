7c5847aa3f4dba781b974958d495717fe6da2c3e
Gagan714 <gaganursmg@gmail.com>
2024-05-08 16:04:04 +0530

    error took 3days still dont know wt changes corrected it finally done

---
+++ chaiwithsky/app/api/razorpay/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { validatePaymentVerification } from "razorpay/dist/utils/razorpay-utils";
+import Payment from "@/models/Payment";
+import connectDB from "@/db/connectDB";
+
+export const POST=async (req)=>{
+    await connectDB()
+    let body=await req.formData()
+    body=Object.fromEntries(body)
+    let p=Payment.findOne({oid:body.razorpay_order_id})
+    if(!p){
+        return NextResponse.json({success:false,message:"order id not found"})
+    }
+    let xx=validatePaymentVerification({"order_id": body.razorpay_order_id,"payment_id":body.razorpay_payment_id},body.razorpay_signature,process.env.KEY_SECRET)
+
+    if(xx){
+        const updatedPayment=await Payment.findOneAndUpdate({oid:body.razorpay_order_id},{done:"true"},{new:true})
+        return NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL}/${updatedPayment.to_user}?paymentdone=true`)
+    }
+    else{
+        return NextResponse.json({success:false,message:"payment verification failed"})
+    }
+}
