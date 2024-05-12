e137f2f48f58a4338fb6dd71248a0e477b8fb74c
Gagan714 <gaganursmg@gmail.com>
2024-05-12 14:16:49 +0530

    final commit

--- chaiwithsky/db/connectDB.js
+++ chaiwithsky/db/connectDB.js
@@ -1,20 +1,15 @@
 import mongoose from "mongoose";
+
 const connectDB = async () => {
-try {
-const conn = await mongoose.connect(`mongodb://localhost:27017/chai`,{
-useNewUrlParser: true,
-});
-console.log("MongoDB Connected: (conn.connection.host}");
-
-} catch (error) {
-
-console.error(error.message);
-
-
-process.exit(1);
-
-}
-
-}
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+        });
+        console.log("MongoDB Connected: ", conn.connection.host);
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    }
+};
 
 export default connectDB;
