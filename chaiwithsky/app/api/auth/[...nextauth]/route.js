99531da9de3fd493f60e9e8d92922c688668114b
Gagan714 <gaganursmg@gmail.com>
2024-05-13 09:56:32 +0530

    commit

--- chaiwithsky/app/api/auth/[...nextauth]/route.js
+++ chaiwithsky/app/api/auth/[...nextauth]/route.js
@@ -5,8 +5,7 @@ import NextAuth from 'next-auth'
 // import EmailProvider from 'next-auth/providers/email'
 import GitHubProvider from "next-auth/providers/github";
 import mongoose from 'mongoose';
-import User from '@/models/User';
-import Payment from '@/models/Payment';
+import User from '../models/User';
 import connectDB from '@/db/connectDB';
 
 export const authoptions= NextAuth({
