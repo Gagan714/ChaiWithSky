2748171212fc8a87902e8f8b586c864cc97940bb
Gagan714 <gaganursmg@gmail.com>
2024-05-13 12:41:40 +0530

    gg

--- chaiwithsky/components/Footer.js
+++ chaiwithsky/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
+    
     <div className="flex justify-center items-center bg-gray-900 text-xs md:text-lg text-white h-9">
         <div>CopyRight &copy; 2024, Chai With Sky - All Rights Reserved </div>
     </div>
